c4bd70c07293ca17be457acbc69a18d7f58ce4ac
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-08-30 02:26:40 -0500

    feat: update wedding invitation with new assets, styles, and audio functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './app.component.html',
 })
-export class App {
-  protected readonly title = signal('world');
+export class App implements AfterViewInit {
+  faPlay = faPlay;
+  faPause = faPause;
+
+  isPlaying = false;
+  showOverlay = true; 
+
+  @ViewChild('player') playerRef!: ElementRef<HTMLAudioElement>;
+
+  ngAfterViewInit() {
+    const player = this.playerRef.nativeElement;
+    player.loop = true;  
+    player.volume = 0;    
+  }
+
+  toggleMusic() {
+    const player = this.playerRef.nativeElement;
+
+    if (!this.isPlaying) {
+      player.volume = 1; 
+      player.play();
+      this.showOverlay = false; 
+    } else {
+      player.pause();
+    }
+
+    this.isPlaying = !this.isPlaying;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom  } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(FontAwesomeModule),
   ]
 };
